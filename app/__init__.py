1d561aab000d10ebd65341b4f5466e08f3dfb8ff
lixuan <455636959@qq.com>
2023-06-12 17:22:04 +0800

    'test'

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask,json,request
 from flask_cors import CORS
+
+from utils.openai import create_openai
+
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 @app.route('/ps_api')
 def index():
     data1 = request.get_data()
     data = json.loads(data1)
-    uuid = data.get('uuid')
-    current_id = data.get('current_id')
-    return 'Hello %s and %s!' % (uuid,current_id)
+    content = data.get('content')
+
+    res = create_openai(content)
+    return 'Hello %s!' % res
 
---
+++ utils/openai.py
@@ -0,0 +1,23 @@
+
+import json
+import openai
+
+def get_api_key():
+    '''
+    {"api": "你的 api keys"}
+    '''
+    openai_key_file = './openai_key.json'
+    with open(openai_key_file, 'r', encoding='utf-8') as f:
+        openai_key = json.loads(f.read())
+    return openai_key['api']
+
+
+def create_openai(prompt):
+    openai.api_key = "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"
+    response = openai.ChatCompletion.create(
+        # text-davinci-003 是指它的模型
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    message = response.get("choices")[0]["message"]["content"]
+    return message
