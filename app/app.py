ef790eb5fab060dd045e28c809311e8c7a58ab33
lixuan <455636959@qq.com>
2023-06-12 16:55:43 +0800

    'config'

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 
-from flask import Flask,json,request
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app, supports_credentials=True)
-@app.route('/ps_api')
-def index():
-    data1 = request.get_data()
-    data = json.loads(data1)
-    uuid = data.get('uuid')
-    current_id = data.get('current_id')
-    return 'Hello %s and %s!' % (uuid,current_id)
-
-if __name__ == '__main__':
-    app.run()
+# from flask import Flask,json,request
+# from flask_cors import CORS
+# app = Flask(__name__)
+# CORS(app, supports_credentials=True)
+# @app.route('/ps_api')
+# def index():
+#     data1 = request.get_data()
+#     data = json.loads(data1)
+#     uuid = data.get('uuid')
+#     current_id = data.get('current_id')
+#     return 'Hello %s and %s!' % (uuid,current_id)
+#
+# if __name__ == '__main__':
+#     app.run()
